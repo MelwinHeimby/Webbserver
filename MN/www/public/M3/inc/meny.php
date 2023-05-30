9f325e68f3c512c674ad2265a38d587816455dcd
MelwinHeimby <04heme@skola.engelholm.se>
2023-05-30 16:33:31 +0200

    jjgh

--- MN/www/public/M3/inc/meny.php
+++ MN/www/public/M3/inc/meny.php
@@ -1,22 +1,16 @@
-<?php if (session_status() === PHP_SESSION_NONE)
-{
-	session_start();
-}
-?>
+<?php if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+} ?>
 
-
-<h1>
-    Innehåll
-</h1>
+<h1>Innehåll</h1>
 <ul>
-	<li><a href="index.php">Hem</a></li>
-	<li><a href="pages/blogg.php">Blogg</a></li>
-	<li><a href="pages/bilder.php">Bilder</a></li>
-	<li><a href="pages/Kontakt.php">Kontakt</a></li>
-	<?php 
-		if (isset($_SESSION['Logged']))
-		{
-			echo '<li><a href="klotter.php">Klotter</a></li>';
-		}
-	?>
+    <li><a href="?pages=start">Hem</a></li>
+    <li><a href="?pages=blogg">Blogg</a></li>
+    <li><a href="?pages=bilder">Bilder</a></li>
+    <li><a href="?pages=Kontakt">Kontakt</a></li>
+    <?php 
+        if (isset($_SESSION['Logged'])) {
+            echo '<li><a href="klotter.php">Klotter</a></li>';
+        }
+    ?>
 </ul>
--- MN/www/public/M3/pages/blogg.php
+++ MN/www/public/M3/pages/blogg.php
@@ -11,61 +11,27 @@
 	<title>Länka in sidor med PHP</title>
 	<link href="css/styleSheet.css" rel="stylesheet" type="text/css">
 </head>
-<header>
-	<?php if ($_SESSION['Logged']){
-		echo $_SESSION['user'];
-	}?>
-</header>
 
 <body>
-	<div id="wrapper">
-		<header>
-      		<h1>Webbserverprogrammering 1 med Bygren</h1>
-      	</header>
-		<!-- header -->
-
-		<section id="leftColumn">
-			<nav>
-	        	<h1>Innehåll</h1>
-				<ul>
-					<li><a href="index.php">Hem</a></li>
-					<li><a href="blogg.php">Blogg</a></li>
-					<li><a href="bilder.php">Bilder</a></li>
-					<li><a href="Kontakt.php">Kontakt</a></li>
-				</ul>
-	        </nav>
-			<aside>
-				<h1>Namn</h1>
-				<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-				Nulla tristique elementum nisl, et vehicula neque laoreet ut. 
-				Praesent sed ultrices purus.
-				</p>
-		  	</aside>
-		</section>
-		<!-- End leftColumn -->
-
-		<main role="main">
+	<main role="main">
 		<section>
-			<hgroup>
-				<h1>Min blogg Nu</h1>
-				<h2>I dag gör vi mallar</h2>
-			</hgroup>
-			<p>Lorem ipsum id duo autem qualisque, cu laoreet eleifend cum. Eu sea 
-					vidit altera civibus, harum deserunt argumentum ne vim. Eu paulo 
-					corpora appareat pro, brute everti iuvaret ne vis. Diam incorrupte 
-					voluptatibus cu vel, nam congue soluta praesent at.
-			</p>    
-	  	</section>
-		</main>
+		    <h1 id="titel">Blogg</h1>
+			<p id="p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tristique elementum nisl, et vehicula neque laoreet ut. Praesent sed ultrices purus.</p>
+		</section>
+	</main>
 		<!-- End main -->
-
-		<footer>
-        	<span id="footerLeft">© Sidlayoutarna</span>
-			<span id="footerRight"><a href="mailto:xxx@doman">xxx@doman</a></span>
-      </footer>
-		<!-- End footer -->
-	</div>
-	<!-- End wrapper -->
 </body>
 </html>
+<style lang="scss">
+	#titel {
+		justify-content: left;
+		position: relative;
+		right: 50%;
+	}
+
+    #p {
+		justify-content: left;
+		position: relative;
+		right: 50%;
+	}
+</style>
